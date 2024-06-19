99039a4f25d5074e4de1f8feab4b2493758d2a01
Danil <danil0316@mail.ru>
2024-06-19 23:47:08 +0500

    added spring

---
+++ webmail.spring.server/src/test/java/com/example/webmail/WebmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
